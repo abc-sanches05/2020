17788c209eeadc37590138eb2d2f349c146d4459
alex dor <abc-sanches05@ramvbler.ru>
2020-06-17 00:23:41 +0300

    17_06

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,24 @@
 'use strict';
-// javascript
 
-//alert('test_js');
-/* let answer = confirm('yes or no');
-console.log(answer);
-*/
-let answer = prompt('yes or no', 'yes');
-console.log(typeof (answer));
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
 
 
-
-
+alert(appData.budget / 30);

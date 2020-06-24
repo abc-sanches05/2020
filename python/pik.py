46f1b347d18bb7d601bcb72c718bc9e5a3d85f75
alex dor <abc-sanches05@ramvbler.ru>
2020-06-24 13:32:43 +0300

    work mr

--- python/pik.py
+++ python/pik.py
@@ -1,19 +1,19 @@
 import requests
 import re
 
-responce = requests.get("https://www.pik.ru/sles/search/list?sortBy=price")
+r = requests.get("https://www.mr-group.ru/catalog/apartments/?project=42&min_price=&max_price=")
 #print(responce.headers)
 
+"""
 with open("C:\\Users\\Александр\\Desktop\\python\\log\\varshavskay.txt", "w") as file:
     print(responce.content, file=file, sep="\n")
-
-""" не работает
-res = responce.content
-print(type(res))
-
-search_price = re.search('FlatsResultTable-rowPriceTitle', res)
-print(search_price)
-
 """
+r_e = r.content
+r_e = bytes.decode(r_e, encoding= 'Windows-1251', errors= 'ignore' )
 
 
+with open("C:\\Users\\Александр\\Desktop\\python\\log\\varshavskay_new3.html", "w") as output_file:
+  output_file.write(r_e)
+
+
+print(r_e)

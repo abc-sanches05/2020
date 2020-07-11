9f8ff15214f1f2da89f9ede77df55f5bada853d5
alex dor <abc-sanches05@ramvbler.ru>
2020-07-12 01:23:09 +0300

    12_07

--- python/parse/all_parse.py
+++ python/parse/all_parse.py
@@ -1,4 +1,7 @@
 import os
+print('parse d1')
 os.system('python c:/Users/Александр/Desktop/github/python/parse/mr_d1.py')
+print('parse metropolia')
 os.system('python c:/Users/Александр/Desktop/github/python/parse/mr_metropolia.py')
+print('parse paveleckai')
 os.system('python c:/Users/Александр/Desktop/github/python/parse/mr_pavel.py')
